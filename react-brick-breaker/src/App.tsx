3cd2b00b563a3eef05ca79a5eb7fafb97b0049b6
Ms Eko <jomaduagwu@gmail.com>
2023-12-12 21:39:29 -0600

    code clean-up

--- react-brick-breaker/src/App.tsx
+++ react-brick-breaker/src/App.tsx
@@ -1,7 +1,5 @@
 // import React from 'react';
-// import BrickBreaker from "./components/Testtest";
-// import Test from "./components/Test";
-import New from "./components/New";
+import BrickBreaker from "./components/BrickBreaker";
 
 
 
@@ -10,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       {/* <canvas id="canvas" width={width} height={height}></canvas> */}
-      <New />
+      <BrickBreaker />
       {/* <p>Mouse moves platform &bull; Press any key to pause</p>
       <button onClick={reload}>Play again</button>
       <div id="score"></div> */}
--- react-brick-breaker/src/components/BrickBreaker.tsx
+++ react-brick-breaker/src/components/BrickBreaker.tsx
@@ -1,17 +1,10 @@
-// import statement
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 
-// functional component declaration
-const BrickBreaker = () => {
-  // state and refs declaration
+const BrickBreaker: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const ballRef = useRef({x: 200, y: 150}); // ref for ball position
-
-  // const [x, setX] = useState(200); // starting horizontal position of ball
-  // const [y, setY] = useState(150); // starting vertical position of ball
-  const [dx, setDx] = useState(1); // amount ball should move horizontally
-  const [dy, setDy] = useState(-3); // amount ball should move vertically
+  // state of ball - position, speed and radius
+  const ballRef = useRef({ x: 200, y: 150, dx: 1, dy: -3, radius: 10 });
 
   // variables set in init()
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
@@ -23,264 +16,267 @@ const BrickBreaker = () => {
 
   const paddleH = 10; // paddle height
   const paddleW = 75; // paddle width
-  const [canvasMinX, setCanvasMinX] = useState<number>(0); // minimum canvas x bounds
-  const [canvasMaxX, setCanvasMaxX] = useState<number>(0); // maximum canvas x bounds
-  const [intervalId, setIntervalId] = useState<number | null>(null);
-  const nrows: number = 6; // number of rows of bricks
-  const ncols: number = 6; // number of columns of bricks
+  const nrows = 6; // number of rows of bricks
+  const ncols = 6; // number of columns of bricks
   const brickHeight = 15; // height of each brick
   const brickWidth = 75; // width of each brick
   const padding = 1; // spacing between each brick
 
-  const ballRadius = 10; // size of ball (pixels)
+  const [canvasMinX, setCanvasMinX] = useState<number>(0); // minimum canvas x bounds
+  const [canvasMaxX, setCanvasMaxX] = useState<number>(0); // maximum canvas x bounds
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+
+  const [score, setScore] = useState(0); // store the number of bricks eliminated
+  const [paused, setPaused] = useState(false); // keep track of whether the game is paused or not
+  const [gameReloaded, setGameReloaded] = useState(false); // added to take care of game reload
 
   const brickColors = ["#80CBC4", "#455A64 ", "#FFE0B2", "#A5D6A7", "#D43ED8"];
   const paddleColor = "black";
   const ballColor = "black";
-  const backColor = "lightgrey";
+  const backColor = "grey";
+
+  // setup canvas and mousemove event listener
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    setCtx(context);
+    setWidth(canvas.width); // changed back to width to fix canvas size
+    setHeight(canvas.height);
+    setPaddleX(canvas.width / 2);
+    setCanvasMinX(canvas.getBoundingClientRect().left);
+    setCanvasMaxX(canvas.getBoundingClientRect().right);
+
+    const bricks = initBricks();
+    setBricks(bricks);
+
+    const handleMouseMove = (evt: MouseEvent) => {
+        const mouseX = evt.clientX - canvas.getBoundingClientRect().left;
+        setPaddleX(mouseX - paddleW / 2);
+        };
+        canvas.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            canvas.removeEventListener("mousemove", handleMouseMove);
+        };
+
+  }, [paddleW]);
+
+  const draw = () => {
+    if (paused || !ctx || !canvasRef.current) return;
+    drawBackground();    
+    ctx.fillStyle = ballColor;
+    // draw the ball
+    drawCircle(ballRef.current.x, ballRef.current.y, ballRef.current.radius);
+    ctx.fillStyle = paddleColor;
+    // draw the paddle
+    drawRect(paddleX, canvasRef.current.height - paddleH, paddleW, paddleH);
+
+    // update ball positionx
+    ballRef.current.x += ballRef.current.dx;
+    ballRef.current.y += ballRef.current.dy;
+
+    drawBricks();
+
+    handleBrickCollision();
+
+    handleWallCollision();
+
+    if (!gameReloaded) {
+      requestAnimationFrame(draw);
+    }
+  };
+
+  const drawBackground = () => {
+    if (ctx) {
+        ctx.fillStyle = backColor;
+        ctx.fillRect(0, 0, canvasRef.current?.width, canvasRef.current.height);
+    }
+};
 
 
-  // Initialize an array of bricks to be visible (true)
-  const initBricks = () => {
-    //   const initBricks = (nrows: number, ncols: number) => {
-    const bricks: boolean[][] = [];
-    for (let i = 0; i < nrows; i++) {
-      const row: boolean[] = [];
-      for (let j = 0; j < ncols; j++) {
-        row.push(true);
+  const drawRect = (x: number, y: number, width: number, height: number) => {
+    if (ctx) {
+      ctx.beginPath();
+      ctx.rect(x, y, width, height);
+      ctx.closePath();
+      ctx.fill();
+    }
+  };
+
+  const drawCircle = (x: number, y: number, radius: number) => {
+    if (ctx) {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fill();
+    }
+  };
+
+  const handleBrickCollision = () => {
+    const rowHeight = brickHeight + padding;
+    const colWidth = brickWidth + padding;
+    const row = Math.floor(ballRef.current.y / rowHeight);
+    const col = Math.floor(ballRef.current.x / colWidth);
+    //if so reverse the ball and mark the brick as broken
+    if (
+      ballRef.current.y < nrows * rowHeight &&
+      row >= 0 &&
+      col >= 0 &&
+      bricks[row][col]
+    ) {
+      ballRef.current.dy = -ballRef.current.dy;
+      bricks[row][col] = false;
+      setScore((prevScore) => prevScore + 1); // add a point every time a brick is broken
+    }
+  };
+
+  const handleWallCollision = () => {
+    //contain the ball by rebounding it off the walls of the canvas
+    if (!width || !height || !paddleX || !ballRef.current) {
+      // added null checks
+      return;
+    }
+
+    if (
+      ballRef.current.x + ballRef.current.dx > width ||
+      ballRef.current.x + ballRef.current.dx < 0
+    ) {
+      ballRef.current.dx = -ballRef.current.dx;
+    }
+
+    if (ballRef.current.y + ballRef.current.dy < 0) {
+      ballRef.current.dy = -ballRef.current.dy;
+    } else if (ballRef.current.y + ballRef.current.dy > height - paddleH) {
+      // check if the ball is hitting the paddle and if it is rebound it
+      if (
+        ballRef.current.x > paddleX &&
+        ballRef.current.x < paddleX + paddleW
+      ) {
+        ballRef.current.dy = -ballRef.current.dy;
       }
-      bricks.push(row);
     }
-    return bricks;
-  };
-
-  // Used to draw the ball
-  const drawCircle = (x: number, y: number, r: number) => {
-    // no update needed
-    if (ctx) {
-      ctx.beginPath();
-      ctx.arc(x, y, r, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fill();
+    
+    if (ballRef.current.y + ballRef.current.dy > height) {
+      //game over, so stop the animation
+      stopAnimation();
     }
   };
 
-  // Used to draw each brick & the paddle
-  const drawRect = (x: number, y: number, w: number, h: number) => {
-    // no update needed
-    if (ctx) {
-      ctx.beginPath();
-      ctx.rect(x, y, w, h);
-      ctx.closePath();
-      ctx.fill();
-    }
-  };
+  // game animation
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas || !ctx) return;
 
-  // Clear the screen in between drawing each animation
-  const clearCanvas = () => {
-    if (ctx && width && height) {
-      ctx.clearRect(0, 0, width, height);
-      drawRect(0, 0, width, height);
-    }
-  };
 
+    const id = requestAnimationFrame(draw);
+    setIntervalId(id);
+
+    return () => stopAnimation();
+  }, [ctx, paused, paddleX, gameReloaded]);
+
+
+  // draw bricks
   const drawBricks = () => {
     for (let i = 0; i < nrows; i++) {
       for (let j = 0; j < ncols; j++) {
+        // calculate brick position
+        const brickX = j * (brickWidth + padding) + padding;
+        const brickY = i * (brickHeight + padding) + padding;
+
+        // set the colors to alternate through all colors in brick colors array
         if (ctx) {
           ctx.fillStyle = brickColors[(i + j) % brickColors.length];
         }
         if (bricks[i][j]) {
-          drawRect(
-            j * (brickWidth + padding) + padding,
-            i * (brickHeight + padding) + padding,
-            brickWidth,
-            brickHeight
-          );
+          drawRect(brickX, brickY, brickWidth, brickHeight);
         }
       }
     }
   };
 
-  const updateScoreText = () => {
-    setScore((prevScore) => prevScore + 1);
+  const startAnimation = () => {
+    const id = window.requestAnimationFrame(draw);
+    setIntervalId(id);
   };
 
-  const startAnimation = useCallback(() => {
-    console.log("Start Animation");
-    const id = window.setInterval(draw, 1);
-    setIntervalId(id);
-  }, []);
-  
   const stopAnimation = () => {
-    // no update needed
-    console.log("Stop animation");
-    if (intervalId != null) {
-      window.clearInterval(intervalId);
+    if (intervalId) {
+      window.cancelAnimationFrame(intervalId);
       setIntervalId(null);
     }
   };
 
-  // What to do when the mouse moves within the canvas
-  const onMouseMove = (
-    evt: React.MouseEvent<HTMLCanvasElement, MouseEvent>
-  ) => {
-    if (width && canvasMinX && canvasMaxX) {
-      if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
-        const newPaddleX = Math.max(evt.pageX - canvasMinX - paddleW / 2, 0);
-        setPaddleX(Math.min(width - paddleW, newPaddleX));
+  // Initialize array of bricks to be visible (true)
+  const initBricks = (): boolean[][] => {
+    const bricksArray: boolean[][] = new Array(nrows);
+
+    for (let i = 0; i < nrows; i++) {
+      // for each row of bricks
+      bricksArray[i] = new Array(ncols);
+      for (let j = 0; j < ncols; j++) {
+        bricksArray[i][j] = true;
       }
     }
+    return bricksArray;
   };
 
-  const handleKeyDown = (evt: React.KeyboardEvent) => {
-    evt.preventDefault();
-    pause();
-  };
-
-  const pause = () => {
-    // no update needed
-    if (paused) {
-      startAnimation();
-    } else {
-      stopAnimation();
+  const clear = () => {
+    if (ctx) {
+      ctx.clearRect(0, 0, width, height);
     }
-    setPaused(!paused);
   };
 
   const reload = () => {
-    setScore(0); // resets score to 0 if reloaded
-    // updateScoreText(); // clear the score displayed if reloaded
-    setBricks(initBricks()); // initialize the bricks, set all to visible
+    stopAnimation(); // stop the existing animation before reloading
+    ballRef.current = { x: 200, y: 150, dx: 1, dy: -3, radius: 10 }; // reset the ball position
+    setScore(0); // set score to 0
     setPaused(false); // set paused to false
-    setX(200); // starting horizontal position of ball
-    setY(150); // starting vertical position of ball
-    setDx(1); // amount ball should move horizontally
-    setDy(-3); // amount ball should move vertically
-    startAnimation(); // start the game animation
+    setBricks(initBricks()); // initialize the bricks, set all to visible
+    setGameReloaded(true); // reset the gameReloaded flag
+
+  };
+  useEffect(() => {
+    if (gameReloaded) {
+        startAnimation();
+        setGameReloaded(false); // reset the gameReloaded flag
+    }
+  }, [gameReloaded]);
+
+  const updateScoreText = () => {
+    const scoreElement = document.getElementById("score");
+    if (scoreElement) {
+      scoreElement.textContent = `Score: ${score}`;
+    }
   };
 
-  const draw = useCallback(() => {
-    console.log("Drawing...");
-    console.log("CTX:", ctx);
-    console.log("Width:", width);
-    console.log("Height:", height);
-    console.log("Paddle X:", paddleX);
-    console.log("Ball position (x, y):", x, y);
-    console.log("Ball speed (dx, dy):", dx, dy);
-
-    if (!ctx || !width || !height || !paddleX) return;
-    // Your draw function logic here
-
-    ctx.fillStyle = backColor;
-    clearCanvas();
-    ctx.fillStyle = ballColor;
-    x;
-    //draw the ball
-    drawCircle(x, y, ballRadius);
-    ctx.fillStyle = paddleColor;
-    //draw the paddle
-    drawRect(paddleX, height - paddleH, paddleW, paddleH);
-    drawBricks();
-
-    //check if we have hit a brick
-    const rowHeight = brickHeight + padding;
-    const colWidth = brickWidth + padding;
-    const row = Math.floor(y / rowHeight);
-    const col = Math.floor(x / colWidth);
-    //if so reverse the ball and mark the brick as broken
-    if (y < nrows * rowHeight && row >= 0 && col >= 0 && bricks[row][col]) {
-      setDy(-dy);
-      setBricks((prevBricks) => {
-        const newBricks = [...prevBricks];
-        newBricks[row][col] = false;
-        return newBricks;
-      });
-      //   score++; // add a point every time a brick is broken
-      updateScoreText(); // show the current score
-    }
-
-    //contain the ball by rebouding it off the walls of the canvas
-    if (x + dx > width || x + dx < 0) setDx(-dx);
-
-    if (y + dy < 0) {
-      setDy(-dy);
-    } else if (y + dy > height - paddleH) {
-      // check if the ball is hitting the
-      // paddle and if it is rebound it
-      if (x > paddleX && x < paddleX + paddleW) {
-        setDy(-dy);
-      }
-    }
-    if (y + dy > height) {
-      //game over, so stop the animation
-      stopAnimation();
-      //   alert("Game Over! Your score is: " + score);
-      document.location.reload();
-    }
-    setX((prevX) => prevX + dx);
-    setY((prevY) => prevY + dy);
-  }, [bricks, paddleX]);
-
   useEffect(() => {
-    console.log("useEffect triggered");
-    const canvas = canvasRef.current;
-    const context = canvas?.getContext("2d");
-
-    console.log("Canvas:", canvas);
-    console.log("Context:", context);
-    console.log("Width:", width);
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      event.preventDefault();
-      pause();
+    const handleKeyDown = (evt: KeyboardEvent) => {
+      if (evt.key === " ") {
+        evt.preventDefault();
+        setPaused((prevPaused) => !prevPaused);
+      }
     };
 
-    if (canvas && context) {
-      setCtx(context);
-      setWidth(canvas.offsetWidth);
-      setHeight(canvas.offsetHeight);
-      setPaddleX(canvas.offsetWidth / 2); // updated to use canvas.offsetWidth
-      setCanvasMinX(canvas.offsetLeft);
-      setCanvasMaxX(canvas.offsetLeft + canvas.offsetWidth);
-      setBricks(initBricks());
-      draw();
-      startAnimation();
-    }
-
-    // attach event listener
     document.addEventListener("keydown", handleKeyDown);
-    // const animationFrame = requestAnimationFrame(draw);
 
     return () => {
-      //   cancelAnimationFrame(animationFrame);
       document.removeEventListener("keydown", handleKeyDown);
-      stopAnimation();
     };
-  }, [canvasRef.current, startAnimation, draw, stopAnimation]);
-
-  //   }, [width, height, paddleX, startAnimation, handleKeyDown, draw, stopAnimation]);
+  }, [paused]);
 
   return (
     <div>
       <h1>Brick Breaker</h1>
-      <canvas
-        ref={canvasRef}
-        width={width || 500}
-        height={height || 300}
-        onMouseMove={onMouseMove}
-        onKeyDown={handleKeyDown}
-        tabIndex={0}
-      />
+      <canvas ref={canvasRef} width="500" height="300"></canvas>
       <p>Mouse moves platform &bull; Press any key to pause</p>
       <button onClick={reload}>Play again</button>
-      <div id="score">Score: {score}</div>
+      <div id="score">Score: {score} </div>
     </div>
   );
 };
 
 export default BrickBreaker;
-
-// function useCallback(arg0: () => void) {
-//     throw new Error("Function not implemented.");
-// }
